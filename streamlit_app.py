e355d6e086687d6131fde2089cf9c4f1a2ec8386
Gregg Saksefski <133022440+turtlewi9@users.noreply.github.com>
2023-05-10 16:03:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,12 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = requests.get("SELECT * from fruit_load_list")
+# New Section to add fruit to list
+streamlit.header('What Fruit do you want to add to list')
+add_my_fruit = streamlit.text_input('What Fruit do you want to add to list?', 'Kiwi')
+streamlit.write('The user entered', add_my_fruit)
+my_cur.execute("select * from fruit_load_list" + add_my_fruit)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load contains:")
+streamlit.dataframe(my_data_rows)
+
