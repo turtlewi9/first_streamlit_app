48bd01afe965f96974f917a82155bbca2eda2dec
Gregg Saksefski <133022440+turtlewi9@users.noreply.github.com>
2023-05-10 15:53:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,8 @@ streamlit.text(my_data_row)
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_row)
+
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load contains:")
+streamlit.dataframe(my_data_rows)
