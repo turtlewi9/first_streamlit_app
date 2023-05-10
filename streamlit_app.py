0fa04d764a45493b752e9b9fe6d3efe258937595
Gregg Saksefski <133022440+turtlewi9@users.noreply.github.com>
2023-05-10 16:47:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -11,8 +13,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-#import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,7 +28,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normailized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normailized)
@@ -57,11 +56,11 @@ streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
 
 # New Section to add fruit to list
-streamlit.header('What Fruit do you want to add to list')
-add_my_fruit = streamlit.text_input('What Fruit do you want to add to list?', 'Kiwi')
+#streamlit.header('What Fruit do you want to add to list')
+#add_my_fruit = streamlit.text_input('What Fruit do you want to add to list?', 'Kiwi')
 
-streamlit.write('The user entered', add_my_fruit)
-my_data_rows = my_cur.fetchall() + add_my_fruit
-streamlit.header("The fruit load contains:")
-streamlit.dataframe(my_data_rows)
+#streamlit.write('The user entered', add_my_fruit)
+#my_data_rows = my_cur.fetchall() + add_my_fruit
+#streamlit.header("The fruit load contains:")
+#streamlit.dataframe(my_data_rows)
 
